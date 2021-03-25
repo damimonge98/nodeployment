5284ec773183f4da264523585b5f655aed7c4310
damimonge98 <69829575+damimonge98@users.noreply.github.com>
2021-03-24 21:17:59 -0300

    added handlers options

--- server.js
+++ server.js
@@ -78,7 +78,11 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Authorization, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS');
-  next();
+  if ('OPTIONS' == req.method) {
+    return res.sendStatus(200);
+} else {
+    next();
+}
 });
 
 server.use(passport.initialize());
